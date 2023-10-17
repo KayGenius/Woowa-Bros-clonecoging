24b0f64ef071050c91dfccbc06c5d237c823b745
kimyoungjae <jae2942@gmail.com>
2023-10-18 00:35:37 +0900

    banner animation add

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 5
+    items: 1
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3
+    items: 1
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2
+    items: 1
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
@@ -23,19 +23,29 @@ const responsive = {
 
 
 
-const Baner = () => {
+const Banner = () => {
+  const [text, setText] = useState([])
   return (
     <div className='banner'>
-        <Carousel responsive={responsive}>
-              <div style={{backgroundImage:'url("https://woowahan-cdn.woowahan.com/new_resources/image/banner/5a76ab3c37ea4e7d80d3a6e11b36e0f1.jpg")'}}></div>
-              <div>Item 2</div>
-              <div>Item 3</div>
-              <div>Item 4</div>
-</Carousel>;
+      <Carousel responsive={responsive} >
+        <div className='banner-1'>
+          <video muted='muted' autoplay='autoplay' loop>
+          <source src='https://woowahan-cdn.woowahan.com/new_resources/image/banner/e7cd396be2fb4c24838ea637b79c32dc.mp4' type='video/mp4' />
+          </video>
+           <p className='banner-1-text'>만드는 사람이 수고로우면 <br></br>쓰는사람이 편하다</p>
+        </div>
 
+        <div className='banner-2'>
+          <video muted='muted' autoplay='autoplay' loop>
+            <source src='https://woowahan-cdn.woowahan.com/new_resources/image/banner/ad2b9b0ab00e42a9b801cc862eae372b.mp4' type='video/mp4' />
+          </video>
+        </div>
+        <div className='banner-3'></div>
+        <div className='banner-4'></div>
+      </Carousel>
 
     </div>
   )
 }
 
-export default Baner
+export default Banner
